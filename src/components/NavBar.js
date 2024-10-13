7dc10cd834a1afa0e15a817682f62ef4005df4d6
Frida Henriksen <fridahenriksen@hotmail.com>
2024-10-13 16:39:59 +0200

    added logo component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,37 @@
+import Logo from "@/components/Logo";
 import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
   return (
-    <header className="w-full px-32 py-8 font-medium">
+    <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
         <Link href="/">Home</Link>
         <Link href="/about">About</Link>
         <Link href="/projects">Projects</Link>
         <Link href="/articles">Articles</Link>
       </nav>
-      <h2>Logo</h2>
+      <Logo />
+      <nav>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+        <Link href="/" target={"_blank"}>
+          T
+        </Link>
+      </nav>
     </header>
   );
 };
