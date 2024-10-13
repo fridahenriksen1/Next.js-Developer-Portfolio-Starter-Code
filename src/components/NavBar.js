fc0728be501ada6a88136f1c90fb8e06256a12a6
Frida Henriksen <fridahenriksen@hotmail.com>
2024-10-13 19:20:17 +0200

    Added classes to links in nav

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,24 @@
-import Logo from "@/components/Logo";
 import Link from "next/link";
 import React from "react";
+import Logo from "./Logo";
 
 const CustomLink = ({ href, title, className = "" }) => {
-  return <Link href={href}>{title}</Link>;
+  return (
+    <Link href={href} className={`${className}relative`}>
+      {title}
+      <span className="h-[1px] inline-block w-full bg-dark">&nbsp;</span>
+    </Link>
+  );
 };
 
 const NavBar = () => {
   return (
     <header className="w-full px-32 py-8 font-medium flex items-center justify-between">
       <nav>
-        <CustomLink href="/" title="Home" />
-        <CustomLink href="/about" title="About" />
-        <CustomLink href="/projects" title="Projects" />
-        <CustomLink href="/articles" title="Articles" />
+        <CustomLink href="/" title="Home" className="mr-4" />
+        <CustomLink href="/about" title="About" className="mx-4" />
+        <CustomLink href="/projects" title="Projects" className="mx-4" />
+        <CustomLink href="/articles" title="Articles" className="ml-4" />
       </nav>
 
       <nav>
