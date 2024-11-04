149b0ec0d17e48bb902d442c252bd0cde574f568
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-04 15:03:42 +0100

    changed border color

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ export default function Home() {
               <Image
                 src={profilePic}
                 alt="CodeBucks"
-                className="w-full h-auto border-solid border-2 border-pink-100 hover:border-dotted  rounded-full transition ease-in-out delay-150 duration-300 hover:-translate-y-1 hover:scale-110"
+                className="w-full h-auto border-solid border-2 border-purple-100 hover:border-dotted  rounded-full transition ease-in-out delay-150 duration-300 hover:-translate-y-1 hover:scale-110"
               ></Image>
             </div>
           </div>
