9f163f98427a4af6ddfdfea3613f5ace6931320a
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-10 15:59:09 +0100

    Hire me button

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { CircularText } from "./Icons";
+import Link from "next/link";
 
 const HireMe = () => {
   return (
     <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden">
       <div className="w-48 h-auto flex items-center justify-center relative">
         <CircularText className={"fill-dark animate-spin-slow"}></CircularText>
+
+        <Link
+          href="mailto:fridahenriksen1@gmail.com"
+          className="flex items-center justify-center absolute bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full"
+        >
+          Hire me
+        </Link>
       </div>
     </div>
   );
