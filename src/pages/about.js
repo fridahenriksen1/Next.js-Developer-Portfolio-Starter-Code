5c4f73858f8c129d1e3014804497d6ad926ec8c8
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-10 19:20:25 +0100

    added animated text to the about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,3 +1,5 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
 import Head from "next/head";
 import React from "react";
 
@@ -6,7 +8,16 @@ const about = () => {
     <>
       <Head>
         <title>Fridas Portfolio | About Page</title>
+        <meta
+          name="descripton"
+          content="Frontend Developer and UX/UI Designer with a focus on creating user-friendly, visually engaging digital solutions. Combining a strong foundation in design and front-end development, I bring creativity and technical skill to craft intuitive, seamless experiences that elevate user interactions and deliver lasting impact."
+        />
       </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout>
+          <AnimatedText text="Passion Fuels Purpose!" />
+        </Layout>
+      </main>
     </>
   );
 };
