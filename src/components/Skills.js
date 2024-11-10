45e51c1c58a20b9f4babb1dc0ae7e7c96d818896
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-10 21:52:44 +0100

    cursor pointer at skill

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -7,7 +7,7 @@ const Skills = () => {
       <h2 className="font-bold text-8xl mt-64 w-full text-center">Skills</h2>
       <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
         <motion.div
-          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark"
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light p-8 shadow-dark cursor-pointer"
           whileHover={{ scale: 1.05 }}
         >
           Web
