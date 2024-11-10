26209cae91dcd792604eeb952c97e57334ec3233
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-10 18:26:42 +0100

    about page file

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import React from "react";
+
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Fridas Portfolio | About Page</title>
+      </Head>
+    </>
+  );
+};
+
+export default about;
