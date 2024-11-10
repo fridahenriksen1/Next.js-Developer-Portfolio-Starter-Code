102f63a47001d42f4c74f8fc89e45c1ae3f9a181
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-10 17:48:41 +0100

    changed the witdth and margin-left at the profile pic

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,7 @@ export default function Home() {
       <main className="flex items-center text-dark w-full min-h-screen">
         <Layout className="pt-0">
           <div className="flex items-center justify-between w-full">
-            <div className="w-1/2">
+            <div className="w-2/5 ml-12">
               <Image
                 src={profilePic}
                 alt="CodeBucks"
