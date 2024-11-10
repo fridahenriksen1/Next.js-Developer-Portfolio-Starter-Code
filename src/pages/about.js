52337e75f3653a061357a2bf9109bcb9ca6dd99f
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-10 19:39:03 +0100

    added bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -14,8 +14,36 @@ const about = () => {
         />
       </Head>
       <main className="flex w-full flex-col items-center justify-center">
-        <Layout>
+        <Layout className="pt-16">
           <AnimatedText text="Passion Fuels Purpose!" />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Biography
+              </h2>
+              <p className="font-medium">
+                Hi, I'm Frida Henriksen, a web developer and UI/UX designer with
+                a passion for creating beautiful, functional, and user-centered
+                digital experiences. I'm a newly graduated digital designer from
+                Brobygrafiska, where I specialized in UX/UI design. I completed
+                my studies in May and am now furthering my expertise by studying
+                frontend development at IT-Högskolan, expanding my technical
+                skills to bring designs to life.
+              </p>
+              <p className="my-4 font-medium">
+                I believe that design is about more than just making things look
+                pretty – it's about solving problems and creating intuitive,
+                enjoyable experiences for users.
+              </p>
+              <p className="font-medium">
+                Whether I'm working on a website, mobile app, or other digital
+                product, I bring my commitment to design excellence and
+                user-centered thinking to every project I work on. I look
+                forward to the opportunity to bring my skills and passion to
+                your next project.
+              </p>
+            </div>
+          </div>
         </Layout>
       </main>
     </>
