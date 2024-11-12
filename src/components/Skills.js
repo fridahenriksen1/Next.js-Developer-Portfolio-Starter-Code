0f2d30362695cebc521ad4ad81cd168dc64ed1da
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-12 22:11:49 +0100

    changed position

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -28,7 +28,7 @@ const Skills = () => {
         </motion.div>
         <Skill name="HTML" x="-20vw" y="2vw" />
         <Skill name="CSS" x="-5vw" y="-10vw" />
-        <Skill name="JavaScript" x="-20vw" y="6vw" />
+        <Skill name="JavaScript" x="20vw" y="6vw" />
         <Skill name="ReactJS" x="-0vw" y="12vw" />
         <Skill name="NextJS" x="-20vw" y="-15vw" />
         <Skill name="UX/UI-design" x="32vw" y="-5vw" />
@@ -37,17 +37,17 @@ const Skills = () => {
         <Skill name="Vue.js" x="-11vw" y="18vw" />
         <Skill name="TypeScript" x="32vw" y="18vw" />
         <Skill name="Bootstrap" x="10vw" y="-26vw" />
-        <Skill name="Node.js" x="10vw" y="30vw" />
-        <Skill name="C#" x="25vw" y="5vw" />
+        <Skill name="Node.js" x="10vw" y="25vw" />
+        <Skill name="C#" x="25vw" y="0vw" />
         <Skill name="Photoshop" x="36vw" y="-15vw" />
-        <Skill name="Hotjar" x="-25vw" y="-25vw" />
+        <Skill name="Hotjar" x="-25vw" y="-20vw" />
         <Skill name="Express" x="-10vw" y="-25vw" />
         <Skill name="Illustrator" x="-10vw" y="25vw" />
         <Skill name="Midjourney" x="-28vw" y="20vw" />
         <Skill name="SQL" x="-30vw" y="10vw" />
         <Skill name="After effects" x="30vw" y="10vw" />
         <Skill name="Github" x="14vw" y="17vw" />
-        <Skill name="Scrum" x="23vw" y="26vw" />
+        <Skill name="Scrum" x="23vw" y="22vw" />
       </div>
     </>
   );
