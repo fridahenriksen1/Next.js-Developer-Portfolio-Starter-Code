d40022150495533d1996535f4f59aeab08f95402
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-18 14:35:57 +0100

    Added projects

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -13,9 +13,14 @@ const projects = () => {
           content="Explore projects showcasing expertise in frontend development and UX/UI design. From visually stunning websites to intuitive user experiences, each project highlights creativity, technical proficiency, and a commitment to excellence."
         />
       </Head>
-      <main>
-        <Layout>
+      <main className="w-full mb-16 flex flex-col items-center justify-center">
+        <Layout className="pt-16">
           <AnimatedText text="Imagination inspires, knowledge empowers!" />
+          <div className="grid grid-cols-12 gap-24">
+            <div className="col-span-12">Featured Project</div>
+            <div className="col-span-6">Project-1</div>
+            <div className="col-span-6">Project-2</div>
+          </div>
         </Layout>
       </main>
     </>
