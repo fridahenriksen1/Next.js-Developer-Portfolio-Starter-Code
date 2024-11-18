e638d8dde46da5c1303902fcdd426793c20009c3
Frida Henriksen <fridahenriksen@hotmail.com>
2024-11-18 20:26:40 +0100

    added first project

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,44 @@
 import AnimatedText from "@/components/AnimatedText";
+import { GithubIcon } from "@/components/Icons";
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12">
+      <Link
+        href={link}
+        target="_blank"
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+        <Link
+          href={link}
+          target="_blank"
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+        <p className="my-2 font-medium text-dark">{summary}</p>
+        <div className="mt-2 flex items-center">
+          <Link href={github} target="_blank" className="w-10">
+            <GithubIcon />
+          </Link>
+          <Link href={link} target="_blank" className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg">
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 const projects = () => {
   return (
@@ -17,7 +54,16 @@ const projects = () => {
         <Layout className="pt-16">
           <AnimatedText text="Imagination inspires, knowledge empowers!" />
           <div className="grid grid-cols-12 gap-24">
-            <div className="col-span-12">Featured Project</div>
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Svemestra - App"
+                img={project1}
+                summary="Svemestra is a travel app created as a group project for our final thesis. It helps users discover nature experiences in Sweden with smart mapping and planning tools. Users can vote on destinations, create routes, and plan adventures easily. We designed and coded key features to deliver a seamless, user-friendly experience."
+                link="/"
+                github="/"
+                type="Featured Project"
+              />
+            </div>
             <div className="col-span-6">Project-1</div>
             <div className="col-span-6">Project-2</div>
             <div className="col-span-12">Featured Project</div>
